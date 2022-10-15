0edc4d5eaa85149cf8e30a9cd399536dd1008f4e
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-10-15 21:08:39 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#basic stuff
+sudo apt-get install build-essential -y
+sudo apt --fix-broken install -y && sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev libgit2-dev libpq-dev -y
+
+#install R
+sudo apt update -qq
+sudo apt install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+sudo apt install --no-install-recommends r-base
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo apt install --no-install-recommends r-cran-tidyverse -y
