ab2917be564e641dec33396e6843c6d9388b8350
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-10-15 22:02:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 sudo apt install --no-install-recommends r-base -y
+
+#install R packages
 sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
 sudo apt install --no-install-recommends r-cran-tidyverse -y
 
