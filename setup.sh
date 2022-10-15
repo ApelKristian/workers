f2d49a3b46fa66a873f06b8cd4900cf2268cdcea
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-10-15 22:09:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,4 +17,4 @@ sudo apt install --no-install-recommends r-base -y
 sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
 sudo apt install --no-install-recommends r-cran-tidyverse -y
 
-sudo R -e "install.packages(c("lubridate", "httr2"))"
+sudo R -e 'install.packages(c("lubridate", "httr2"))'
