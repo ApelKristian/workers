da94355e4ffbb8329eb407d8ee3ffb178531053e
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-10-15 21:12:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ sudo apt install --no-install-recommends software-properties-common dirmngr
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-sudo apt install --no-install-recommends r-base
+sudo apt install --no-install-recommends r-base -y
 sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
 sudo apt install --no-install-recommends r-cran-tidyverse -y
